8d9831c7e73888a45246651cc2d90d7a004dfdbe
PERRIER <m1x6me@gmail.com>
2023-07-11 08:57:29 +0200

    config.json modification to find it

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import json
+import os
+import sys
+import tkinter as tk
+from tkinter import filedialog
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 import time
 import threading
 
 
-def refresh_page(url, refresh_time):
-    driver = webdriver.Edge(service=Service(config['edge_driver_path']))
+def refresh_page(url, refresh_time, driver_path):
+    driver = webdriver.Edge(service=Service(driver_path))
     driver.get(url)
 
     while True:
@@ -14,19 +18,37 @@ def refresh_page(url, refresh_time):
         driver.refresh()
 
 
+def get_config_path():
+    root = tk.Tk()
+    root.withdraw()
+
+    config_path = filedialog.askopenfilename()
+
+    return config_path
+
+
 if __name__ == '__main__':
-    # Load configuration from file
-    with open(r'your_file_path\config.json', 'r') as f:
-        config = json.load(f)
+    config_path = get_config_path()
 
-    urls = config['urls']
-    refresh_time = config['refresh_time']
+    if config_path and os.path.isfile(config_path):
+        with open(config_path, 'r') as f:
+            config = json.load(f)
 
-    threads = []
-    for url in urls:
-        t = threading.Thread(target=refresh_page, args=(url, refresh_time))
-        threads.append(t)
-        t.start()
+        urls = config['urls']
+        refresh_time = config['refresh_time']
 
-    for t in threads:
-        t.join()
+        if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+            driver_path = os.path.join(sys._MEIPASS, 'msedgedriver.exe')
+        else:
+            driver_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'msedgedriver.exe')
+
+        threads = []
+        for url in urls:
+            t = threading.Thread(target=refresh_page, args=(url, refresh_time, driver_path))
+            threads.append(t)
+            t.start()
+
+        for t in threads:
+            t.join()
+    else:
+        print("Aucun chemin de fichier de configuration n'a été fourni.")
